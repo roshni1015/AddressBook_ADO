a9d17efbbcbe015528c7b25ebce84a86dd7b33a5
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-13 19:22:37 +0530

    [Roshni]-Create a Table

--- AddressBook_ADO/AddressBook_ADO/Program.cs
+++ AddressBook_ADO/AddressBook_ADO/Program.cs
@@ -3,4 +3,20 @@ using AddressBook_ADO;
 
 Console.WriteLine("Welcome To Address Book ADO");
 AddressBookADO address = new();
-address.Create_Database();
+while (true)
+{
+    Console.WriteLine("Choose the option :\n1)Create a Database\n2)Create table in DB");
+    int option = Convert.ToInt16(Console.ReadLine());
+    switch (option)
+    {
+        case 1:
+            address.Create_Database();
+            break;
+        case 2:
+            address.CreateTable();
+            break;
+        default:
+            Console.WriteLine("Please choose correct option");
+            break;
+    }
+}
