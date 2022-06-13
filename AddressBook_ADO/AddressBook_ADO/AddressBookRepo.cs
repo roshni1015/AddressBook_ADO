8cbadca701a2041fe2d629ff0a1859ad70488e5d
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-13 18:41:16 +0530

    [Roshni]-Create a Table

--- AddressBook_ADO/AddressBook_ADO/AddressBookRepo.cs
+++ AddressBook_ADO/AddressBook_ADO/AddressBookRepo.cs
@@ -25,6 +25,22 @@ namespace AddressBook_ADO
                 Console.WriteLine("Exception occured :" + e.Message + "\t");
             }
         }
+        public void CreateTable()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=LAPTOP-RLUTTHG1; Initial Catalog =master; Integrated Security = True;");
+                Connection.Open();
+                SqlCommand cmd = new SqlCommand("Create table PersonContact(ID int identity(1,1)primary key,FirstName varchar(200),LastName varchar(200),Address varchar(200), City varchar(200), State varchar(200), ZipCode varchar(200), PhoneNumber varchar(50), EmailId varchar(200)); ", Connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("AddressBook table is created!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("exception occured while creating table:" + e.Message + "\t");
+            }
+        }
 
     }
     
