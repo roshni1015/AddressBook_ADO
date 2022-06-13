524bf75dec6f5b990669838308af338284afab08
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-13 18:27:34 +0530

    [Roshni]-Create a Database

---
+++ AddressBook_ADO/AddressBook_ADO/AddressBookRepo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_ADO
+{
+    public class AddressBookADO
+    {
+        public void Create_Database()
+        {
+            try
+            {
+                SqlConnection Connection = new SqlConnection(@"Data Source=LAPTOP-RLUTTHG1; Initial Catalog =master; Integrated Security = True;");
+                Connection.Open();
+                SqlCommand cmd = new SqlCommand("Create database AddressBookForADO;", Connection);
+                cmd.ExecuteNonQuery();
+                Console.WriteLine("AddressbookService Database created successfully!");
+                Connection.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception occured :" + e.Message + "\t");
+            }
+        }
+
+    }
+    
+}
--- AddressBook_ADO/AddressBook_ADO/Program.cs
+++ AddressBook_ADO/AddressBook_ADO/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using AddressBook_ADO;
+
 Console.WriteLine("Welcome To Address Book ADO");
+AddressBookADO address = new();
+address.Create_Database();
+
