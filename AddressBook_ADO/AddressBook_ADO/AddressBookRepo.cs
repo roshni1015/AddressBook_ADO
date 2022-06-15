20505b5432a655dac6827f201829f469a10b3c3c
roshni1015 <roshniadatrao1110@gmail.com>
2022-06-15 07:18:23 +0530

    [Roshni]-Get All Contacts In Alphbetical Order By City

--- AddressBook_ADO/AddressBook_ADO/AddressBookRepo.cs
+++ AddressBook_ADO/AddressBook_ADO/AddressBookRepo.cs
@@ -326,6 +326,50 @@ namespace AddressBook_ADO
                 Console.WriteLine(e.Message);
             }
         }
+        public void GetAllContactsInAlphbeticalOrderByCity()
+        {
+            try
+            {
+                AddressBookModel addressmodel = new AddressBookModel();
+                SqlConnection Connection = new SqlConnection(@"Data Source=LAPTOP-RLUTTHG1; Initial Catalog =AddressBookForADO; Integrated Security = True;");
+                using (this.connection)
+                {
+                    string Query = @"Select * from PersonContact where City='Pune' order by FirstName ASC;";
+                    SqlCommand cmd = new SqlCommand(Query, this.connection);
+                    this.connection.Open();
+                    SqlDataReader datareader = cmd.ExecuteReader();
+                    if (datareader.HasRows)
+                    {
+                        while (datareader.Read())
+                        {
+                            addressmodel.AddressBookId = datareader.GetInt32(0);
+                            addressmodel.FirstName = datareader.GetString(1);
+                            addressmodel.LastName = datareader.GetString(2);
+                            addressmodel.Address = datareader.GetString(3);
+                            addressmodel.City = datareader.GetString(4);
+                            addressmodel.State = datareader.GetString(5);
+                            addressmodel.ZipCode = datareader.GetString(6);
+                            addressmodel.PhoneNumber = datareader.GetString(7);
+                            addressmodel.EmailId = datareader.GetString(8);
+
+                            Console.WriteLine(addressmodel.FirstName + " " +
+                                addressmodel.LastName + " " +
+                                addressmodel.Address + " " +
+                                addressmodel.City + " " +
+                                addressmodel.State + " " +
+                                addressmodel.ZipCode + " " +
+                                addressmodel.PhoneNumber + " " +
+                                addressmodel.EmailId + " "
+                                );
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
 
     }
 }
--- AddressBook_ADO/AddressBook_ADO/Program.cs
+++ AddressBook_ADO/AddressBook_ADO/Program.cs
@@ -5,15 +5,19 @@ Console.WriteLine("Welcome To Address Book ADO");
 AddressBookADO address = new();
 while (true)
 {
-    Console.WriteLine("Choose the option :\n1)Create a Database\n2)Create table in DB\n3)Insert Values to Table\n4)Retrieve values from Table\n6)Update details of Contact\n7)Delete Contacts\n8)Count Of Records by City or State\n9)Get Records by City\n10)Get Records by State ");
+    Console.WriteLine("Choose the option :\n1)Create a Database\n2)Create table in DB\n3)Insert Values to Table\n4)Retrieve values from Table\n6)Update details of Contact\n7)Delete Contacts\n8)Count Of Records by City or State\n9)Get Records by City\n10)Get Records by State\n11)Alphabetically Sorted for given city ");
     int option = Convert.ToInt16(Console.ReadLine());
     switch (option)
     {
         case 1:
             address.Create_Database();
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 2:
             address.CreateTable();
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 3:
             AddressBookModel addressbook = new AddressBookModel();
@@ -27,9 +31,13 @@ while (true)
             addressbook.EmailId = "roshni@gmail.com";
             address.AddContact(addressbook);
             Console.WriteLine("Record Inserted successfully");
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 4:
             address.GetAllContact();
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 5:
             AddressBookModel addressbook1 = new AddressBookModel();
@@ -44,10 +52,13 @@ while (true)
             address.UpdateContact(addressbook1);
             Console.WriteLine("Record Updated successfully");
             Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 6:
             string UpdatedAddress = address.UpdateEmployeeDetails();
             Console.WriteLine(UpdatedAddress);
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 7:
             AddressBookModel addressbook2 = new AddressBookModel();
@@ -55,20 +66,36 @@ while (true)
             address.DeleteContact(addressbook2);
             Console.WriteLine("Record Deleted successfully");
             Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 8:
             int countCity = address.CountOfEmployeeDetailsByCity();
             Console.WriteLine("Count of Records by City= Pune :" + countCity);
+            Console.WriteLine("-----------------------------------------------------------------------------------");
             int CountState = address.CountOfEmployeeDetailsByState();
             Console.WriteLine("Count of Records by State= Maharastra :" + CountState);
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         case 9:
             Console.WriteLine("Get Contacts by City name");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
             address.GetAllContactByCity();
+            Console.WriteLine();
             break;
         case 10:
             Console.WriteLine("Get Contacts by State name");
+            Console.WriteLine("-------------------------------------------------------------------------------------------");
             address.GetAllContactByState();
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
+            break;
+        case 11:
+            Console.WriteLine("Get Contacts by City name in Alphabetical Order");
+            Console.WriteLine("---------------------------------------------------------------------------------------------");
+            address.GetAllContactsInAlphbeticalOrderByCity();
+            Console.WriteLine();
+            Console.WriteLine("********************************************************************************************************");
             break;
         default:
             Console.WriteLine("Please choose correct option");
